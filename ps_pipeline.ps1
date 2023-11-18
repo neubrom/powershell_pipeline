a67e831027040909ba82922b36b7c32bcec91a90
neubrom <epro050@t-online.de>
2023-11-18 21:15:23 +0100

    add GUID branch

--- ps_pipeline.ps1
+++ ps_pipeline.ps1
@@ -1,116 +1,152 @@
-<#
-.SYNOPSIS
-    Copies files based on regex matching from source to target directories, handles duplicates with versioning, calculates and verifies SHA-256 checksums, moves processed files to a backup directory, and logs operations.
 
-.DESCRIPTION
-    This script searches specified source directories for files matching a regex pattern, handles file duplicates by versioning if necessary, and copies them to a target directory. It calculates SHA-256 checksums for these files, compares them with existing checksums if available, moves successfully processed files to a backup directory, and logs all operations including errors.
-
-.PARAMETER sourceDirs
-    Array of paths to the source directories where files are located.
-
-.PARAMETER targetDir
-    Path of the target directory where files will be copied to.
-
-.PARAMETER backupDir
-    Path of the backup directory where successfully processed files will be moved to.
-
-.PARAMETER logFile
-    Path to the log file for recording script operations and events.
-
-.PARAMETER regexPattern
-    The regex pattern used to identify files to be processed.
-
-.EXAMPLE
-    .\scriptName.ps1 -sourceDir "C:\source" -targetDir "C:\target" -backupDir "C:\backup" -logFile "C:\log.txt" -regexPattern "FO\d{8}"
-
-.NOTES
     Author: Roman Poltoratski
     GPL-3.0 License
     Version: 1.1
     Update the sourceDirs, targetDir, backupDir, logFile, and regexPattern variables as per your environment.
+
+
+    <#
+.SYNOPSIS
+    Generates a GUID for a file based on its name, size, and content, and manages backups with versioning.
+
+.DESCRIPTION
+    This script generates a GUID for a given file based on its name, size, and SHA1 hash of its content.
+    It then checks for a specified pattern in the file name. If matched, the file is backed up to a folder
+    named after the pattern and date. It handles versioning for files with the same name but different content.
+
+.PARAMETER FilePath
+    Path of the file to be processed.
+
+.PARAMETER Pattern
+    Pattern to match in the file name for backup.
+
+.PARAMETER BackupFolder
+    Path of the folder where backups are stored.
+
+.EXAMPLE
+    .\ThisScript.ps1 -FilePath "C:\path\to\your\file.txt" -Pattern "your_pattern" -BackupFolder "C:\backup"
+
+.NOTES
+    Version: 1.2
+    GPL-3.0 License
+    Author: Roman Poltoratski
+    Date: 2023-11-18
 #>
 
 param (
-    [string[]]$sourceDirs,
-    [string]$targetDir,
-    [string]$backupDir,
-    [string]$logFile,
-    [string]$regexPattern
+    [string]$FilePath,
+    [string]$Pattern,
+    [string]$BackupFolder
 )
 
-function Get-FileHashSHA256 {
-    param ([string]$filePath)
-    (Get-FileHash -Path $filePath -Algorithm SHA256).Hash
-}
-
-function Write-Log {
+function Generate-GuidFromFile {
     param (
-        [string]$message,
-        [string]$logFile
-    )
-    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    "$timestamp: $message" | Add-Content -Path $logFile
-}
-
-function Copy-FilesBasedOnPattern {
-    param (
-        [string]$sourceDir,
-        [string]$targetDir,
-        [string]$pattern,
-        [string]$logFile,
-        [string]$backupDir
+        [string]$FilePath
     )
 
-    $files = Get-ChildItem -Path $sourceDir -Recurse | Where-Object { $_.Name -match $pattern }
+    if (-Not (Test-Path $FilePath)) {
+        Write-Error "File not found: $FilePath"
+        return $null
+    }
 
-    foreach ($file in $files) {
-        $targetFilePath = Join-Path $targetDir $file.Name
-        $fileHash = Get-FileHashSHA256 -filePath $file.FullName
+    try {
+        $file = Get-Item $FilePath
+        $fileName = $file.Name
+        $fileSize = $file.Length
+        $sha1 = New-Object System.Security.Cryptography.SHA1CryptoServiceProvider
+        $fileContent = [System.IO.File]::ReadAllBytes($FilePath)
+        $hashBytes = $sha1.ComputeHash($fileContent)
+        $hashString = -join ($hashBytes | ForEach-Object { $_.ToString("x2") })
+        $combinedString = "$fileName$fileSize$hashString"
+        $combinedBytes = [System.Text.Encoding]::UTF8.GetBytes($combinedString)
+        $finalHashBytes = $sha1.ComputeHash($combinedBytes)
+        $finalHashString = -join ($finalHashBytes | ForEach-Object { $_.ToString("x2") })
+        $guidString = $finalHashString.Substring(0, 32)
+        $guid = [guid]::new($guidString)
+        return $guid
+    } catch {
+        Write-Error "An error occurred while generating GUID: $_"
+        return $null
+    }
+}
 
-        if (Test-Path -Path $targetFilePath) {
-            $targetFileHash = Get-FileHashSHA256 -filePath $targetFilePath
+function Backup-FileWithVersioning {
+    param (
+        [string]$SourceFilePath,
+        [string]$DestinationFolder,
+        [string]$GUID
+    )
 
-            if ($fileHash -ne $targetFileHash) {
-                $newFileName = Get-VersionedFileName -sourceDir $targetDir -fileName $file.Name
-                $targetFilePath = Join-Path $targetDir $newFileName
+    try {
+        $fileName = [System.IO.Path]::GetFileName($SourceFilePath)
+        $backupFilePath = Join-Path $DestinationFolder $fileName
+        $backupGuidFilePath = $backupFilePath + ".GUID"
+
+        $version = 0
+        while (Test-Path $backupFilePath -or Test-Path $backupGuidFilePath) {
+            if ((Get-Content $backupGuidFilePath) -ne $GUID) {
+                $version++
+                $backupFilePath = Join-Path $DestinationFolder ($fileName -replace "\.([^.]+)$", ("v" + $version + ".$1"))
+                $backupGuidFilePath = $backupFilePath + ".GUID"
+            } else {
+                break
             }
         }
 
-        Copy-Item -Path $file.FullName -Destination $targetFilePath
-        $copiedFileHash = Get-FileHashSHA256 -filePath $targetFilePath
+        Copy-Item $SourceFilePath $backupFilePath
+        $GUID | Out-File $backupGuidFilePath
+        return $true
+    } catch {
+        Write-Error "An error occurred while backing up the file: $_"
+        return $false
+    }
+}
 
-        if ($fileHash -eq $copiedFileHash) {
-            Write-Log "Successfully copied and verified file to $targetFilePath" -logFile $logFile
-            $backupFilePath = Join-Path $backupDir $file.Name
-            Move-Item -Path $file.FullName -Destination $backupFilePath
-            Write-Log "Moved source file to backup: $backupFilePath" -logFile $logFile
-        } else {
-            Write-Log "Hash mismatch after copying file to $targetFilePath" -logFile $logFile
+function Test-BackupIntegrity {
+    param (
+        [string]$BackupFolder
+    )
+
+    $backupFiles = Get-ChildItem $BackupFolder -Recurse | Where-Object { -not $_.PSIsContainer }
+    foreach ($file in $backupFiles) {
+        if ($file.Extension -eq ".GUID") {
+            continue
+        }
+
+        $guidFilePath = $file.FullName + ".GUID"
+        if (-not (Test-Path $guidFilePath)) {
+            Write-Host "Missing GUID file for: $($file.FullName)"
+            continue
+        }
+
+        $originalGuid = Get-Content $guidFilePath
+        $computedGuid = Generate-GuidFromFile -FilePath $file.FullName
+        if ($originalGuid -ne $computedGuid) {
+            Write-Host "Integrity check failed for: $($file.FullName)"
         }
     }
 }
 
-function Get-VersionedFileName {
-    param (
-        [string]$sourceDir,
-        [string]$fileName
-    )
+# Main script logic
+$guid = Generate-GuidFromFile -FilePath $FilePath
+if ($guid -eq $null) {
+    Write-Host "Exiting due to error in GUID generation."
+    exit
+}
 
-    $version = 1
-    $fileBaseName = [System.IO.Path]::GetFileNameWithoutExtension($fileName)
-    $fileExtension = [System.IO.Path]::GetExtension($fileName)
-    $newFileName = "${fileBaseName}_v$version$fileExtension"
-
-    while (Test-Path -Path (Join-Path $sourceDir $newFileName)) {
-        $version++
-        $newFileName = "${fileBaseName}_v$version$fileExtension"
+if ($FilePath -match $Pattern) {
+    $dateFolder = (Get-Date).ToString("yyyy-MM-dd")
+    $dateSpecificBackupFolder = Join-Path $BackupFolder $dateFolder
+    if (-Not (Test-Path $dateSpecificBackupFolder)) {
+        New-Item -ItemType Directory -Path $dateSpecificBackupFolder
     }
 
-    return $newFileName
+    $backupSuccess = Backup-FileWithVersioning -SourceFilePath $FilePath -DestinationFolder $dateSpecificBackupFolder -GUID $guid
+    if (-not $backupSuccess) {
+        Write-Host "Exiting due to error in backup process."
+        exit
+    }
 }
 
-foreach ($sourceDir in $sourceDirs) {
-    Copy-FilesBasedOnPattern -sourceDir $sourceDir -targetDir $targetDir -pattern $regexPattern -logFile $logFile -backupDir $backupDir
-}
-
-Write-Log "Script execution completed." -logFile $logFile
+# Optional: Perform integrity check on the backups
+# Test-BackupIntegrity -BackupFolder $BackupFolder

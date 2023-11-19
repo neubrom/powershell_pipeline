08c16ee1bc9565466f969bfda59f599136ee183a
neubrom <epro050@t-online.de>
2023-11-19 17:31:25 +0100

    update test and yml

--- test/ps_pipeline.Tests.ps1
+++ test/ps_pipeline.Tests.ps1
@@ -1,34 +1,36 @@
-# Import the module/script containing the functions to be tested
+# Import the PowerShell script to be tested
 . "$PSScriptRoot\..\ps_pipeline.ps1"
 
-# Describe block defines a group of test cases
+# Describe block for the group of tests
 Describe "PowerShell Script ps_pipeline Tests" {
 
-    # BeforeEach block runs before each test in the Describe block
+    # BeforeEach block to set up the environment for each test
     BeforeEach {
-        # Setup test directories
-        $testSourceDir = Join-Path $Env:GITHUB_WORKSPACE "testSource"
-        $testTargetDir = Join-Path $Env:GITHUB_WORKSPACE "testTarget"
-        $testBackupDir = Join-Path $Env:GITHUB_WORKSPACE "testBackup"
+        # Define the source, target, and backup directories
+        $testSourceDir = Join-Path $Env:TEST_SOURCE_DIR "testSource"
+        $testTargetDir = Join-Path $Env:TEST_TARGET_DIR "testTarget"
+        $testBackupDir = Join-Path $Env:TEST_BACKUP_DIR "testBackup"
+
+        # Create the directories
         New-Item -ItemType Directory -Path $testSourceDir, $testTargetDir, $testBackupDir -Force
 
         # Create a test file in the source directory
         $testFile = Join-Path $testSourceDir "testFile.txt"
         "Test content" | Out-File -FilePath $testFile
-
-        # Define other necessary variables or parameters
-        $testRegexPattern = ".*" # Example regex pattern
     }
 
-    # Test case
+    # Test case for verifying the file processing and movement
     It "Should process the file correctly and move it to the backup folder" {
-        # Call the script with the test parameters
-        ps_pipeline -SourceFolder $testSourceDir -RegexPattern $testRegexPattern -TargetFolder $testTargetDir -BackupFolder $testBackupDir
+        # Run the PowerShell script with parameters
+        ps_pipeline -SourceFolder $testSourceDir -TargetFolder $testTargetDir -BackupFolder $testBackupDir -RegexPattern ".*"
 
-        # Assertions to verify the expected outcomes
+        # Assertions to check the expected outcomes
+        # Check if the file exists in the backup folder
         $backupFilePath = Join-Path $testBackupDir "testFile.txt"
         $fileExistsInBackup = Test-Path $backupFilePath
         $fileExistsInBackup | Should -BeTrue
+
+        # Check if the file no longer exists in the source folder
         $fileExistsInSource = Test-Path $testFile
         $fileExistsInSource | Should -BeFalse
     }

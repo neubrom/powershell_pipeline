376ae628832f82e07f78a64e7ef842c387b6dfe9
neubrom <epro050@t-online.de>
2023-11-19 17:21:36 +0100

    update test and yml

--- test/ps_pipeline.Tests.ps1
+++ test/ps_pipeline.Tests.ps1
@@ -1,59 +1,35 @@
-# Dot-source the PowerShell script to test its functions
-. .\ps_pipeline.ps1
+# Import the module/script containing the functions to be tested
+. "$PSScriptRoot\..\ps_pipeline.ps1"
 
-# Test setup: creating necessary directories and files
-$testSourceDir = Join-Path $env:GITHUB_WORKSPACE "testSource"
-$testTargetDir = Join-Path $env:GITHUB_WORKSPACE "testTarget"
-$testBackupDir = Join-Path $env:GITHUB_WORKSPACE "testBackup"
-$testLogFile = Join-Path $env:GITHUB_WORKSPACE "test.log"
-$testFileName = "testFile.txt"
-$testFilePath = Join-Path $testSourceDir $testFileName
+# Describe block defines a group of test cases
+Describe "PowerShell Script ps_pipeline Tests" {
 
-BeforeAll {
-    New-Item -ItemType Directory -Path $testSourceDir, $testTargetDir, $testBackupDir -Force
-    "Test content" | Out-File -FilePath $testFilePath
-}
+    # BeforeEach block runs before each test in the Describe block
+    BeforeEach {
+        # Setup test directories
+        $testSourceDir = Join-Path $Env:GITHUB_WORKSPACE "testSource"
+        $testTargetDir = Join-Path $Env:GITHUB_WORKSPACE "testTarget"
+        $testBackupDir = Join-Path $Env:GITHUB_WORKSPACE "testBackup"
+        New-Item -ItemType Directory -Path $testSourceDir, $testTargetDir, $testBackupDir -Force
 
-Describe "Get-FileHashSHA256 Tests" {
-    It "Calculates the correct SHA-256 hash for a known file" {
-        $knownHash = "expected-sha256-hash-of-testFile.txt" # Replace with actual hash
-        $calculatedHash = Get-FileHashSHA256 -filePath $testFilePath
-        $calculatedHash | Should -Be $knownHash
-    }
-}
+        # Create a test file in the source directory
+        $testFile = Join-Path $testSourceDir "testFile.txt"
+        "Test content" | Out-File -FilePath $testFile
 
-Describe "Write-Log Tests" {
-    It "Writes the correct log message to a file" {
-        $testMessage = "Test log entry"
-        Write-Log -message $testMessage -logFile $testLogFile
-        $logContent = Get-Content -Path $testLogFile
-        $logContent | Should -Contain $testMessage
-    }
-}
-
-Describe "Copy-FilesBasedOnPattern Tests" {
-    It "Successfully copies and verifies files based on pattern" {
-        Copy-FilesBasedOnPattern -sourceDir $testSourceDir -targetDir $testTargetDir -pattern ".*\.txt" -logFile $testLogFile -backupDir $testBackupDir
-        Test-Path (Join-Path $testTargetDir $testFileName) | Should -BeTrue
+        # Define other necessary variables or parameters
+        $testRegexPattern = ".*" # Example regex pattern
     }
 
-    It "Moves the source file to backup after successful copy" {
-        Test-Path (Join-Path $testBackupDir $testFileName) | Should -BeTrue
-    }
-}
+    # Test case
+    It "Should process the file correctly and move it to the backup folder" {
+        # Call the script with the test parameters
+        ps_pipeline -SourceFolder $testSourceDir -RegexPattern $testRegexPattern -TargetFolder $testTargetDir -BackupFolder $testBackupDir
 
-Describe "Get-VersionedFileName Tests" {
-    It "Generates a correctly versioned file name" {
-        $existingFileName = "existingFile.txt"
-        $existingFilePath = Join-Path $testSourceDir $existingFileName
-        New-Item -Path $existingFilePath -ItemType File
-        $versionedName = Get-VersionedFileName -sourceDir $testSourceDir -fileName $existingFileName
-        $versionedName | Should -Be "existingFile_v1.txt"
-        Remove-Item -Path $existingFilePath
+        # Assertions to verify the expected outcomes
+        $backupFilePath = Join-Path $testBackupDir "testFile.txt"
+        $fileExistsInBackup = Test-Path $backupFilePath
+        $fileExistsInBackup | Should -BeTrue
+        $fileExistsInSource = Test-Path $testFile
+        $fileExistsInSource | Should -BeFalse
     }
-}
-
-# Test cleanup: removing test directories and files
-AfterAll {
-    Remove-Item -Path $testSourceDir, $testTargetDir, $testBackupDir, $testLogFile -Recurse -Force
 }
